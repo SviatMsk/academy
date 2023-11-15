f24ffba2b532687929dfcbd27075a79828d57d1c
WC <slava.sviat2023@gmail.com>
2023-11-15 18:45:47 +0300

    *

---
+++ src/by/academy/homework1/Zadacha2.java
@@ -0,0 +1,34 @@
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Zadacha2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите тип данных");
+        String tip = sc.next();
+        System.out.println("Введите переменную");
+        double x = sc.nextDouble();
+        switch (tip) {
+            case "int":
+                System.out.println(x % 2);
+                break;
+                case "double":
+                    System.out.println(x * 70 / 100);
+                    break;
+            case "float":
+                System.out.println(x * x);
+                break;
+            case "char":
+                System.out.println("x".charAt(0));
+                break;
+            case "String":
+                System.out.println("Hello " + x);
+                break;
+                default:
+                    System.out.println("Unsupported type");
+        }
+
+            sc.close();
+        }
+    }
