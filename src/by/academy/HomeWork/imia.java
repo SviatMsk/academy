9e0c02b5b3d28c15d6f7c9ab1e07d03132fc0435
WC <slava.sviat2023@gmail.com>
2023-11-18 14:03:51 +0300

    del

--- src/by/academy/HomeWork/imia.java
+++
@@ -1,9 +0,0 @@
-package by.academy.HomeWork;
-
-public class imia {
-    public static void main(String[] args){
-        String x = "Святослав";
-        String y = " Дарова";
-        System.out.println(x + y);
-    }
-}
